1b83b98029a2d54ba346a097a809e86327c8d4f3
KooMinJeong <cookie00421@gmail.com>
2025-03-08 14:09:20 +0900

    ISSUE-9 홈 페이지 구현 (#7)
    * feat: 홈 페이지 구현
    
    * fix: Button 컴포넌트 className props로 받도록 수정

--- src/_components/Button/icon.tsx
+++ src/_components/Button/icon.tsx
@@ -1,21 +1,29 @@
-import Button from 'src/_components/Button';
-import IconCamera from 'src/assets/icon-camera';
+import Button from "src/_components/Button";
+import { cn } from "src/_lib/utils";
+import IconCamera from "src/assets/icon-camera";
 
 interface IconProps {
   children?: React.ReactNode;
-  disabled?: boolean
+  disabled?: boolean;
   onClick?: () => void;
+  className?: string;
 }
 
-const Icon = ({ children, disabled = false, onClick }: IconProps) => {
+const Icon = ({ children, disabled = false, onClick, className }: IconProps) => {
   return (
-    <Button.Default variant="default" size={'large'} className='label-md' disabled={disabled} onClick={onClick}>
+    <Button.Default
+      variant="default"
+      size={"large"}
+      className={cn("label-md", className)}
+      disabled={disabled}
+      onClick={onClick}
+    >
       <div className="bg-gray-40">
         <IconCamera width={32} height={32} />
       </div>
       {children}
     </Button.Default>
-  )
-}
+  );
+};
 
 export default Icon;
