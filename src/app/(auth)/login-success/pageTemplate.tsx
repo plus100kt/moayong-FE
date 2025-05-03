a24656b18fc2cafea94a383cb49e4e75777b019e
itaebro <plus100kt@gmail.com>
2025-05-03 20:05:22 +0900

    fix: login-success onboarding 처리

--- src/app/(auth)/login-success/pageTemplate.tsx
+++ src/app/(auth)/login-success/pageTemplate.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { LoaderPinwheel } from "lucide-react";
 import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import { getMe } from "src/_api/api";
 import { useQuery } from "@tanstack/react-query";
 import { AxiosError } from "axios";
@@ -9,6 +9,8 @@ import type { UserResponse } from "src/_types/type";
 
 const LoginSuccessPage = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const role = searchParams.get("role");
 
   const {
     data: dataUser,
@@ -19,12 +21,21 @@ const LoginSuccessPage = () => {
     queryKey: ["get-user"],
     queryFn: () => getMe(),
     retry: false,
+    enabled: role !== "ONBOARDING", // ONBOARDING이면 요청 자체를 막음
   });
+
+  useEffect(() => {
+    if (role === "ONBOARDING") {
+      router.push("/register");
+    }
+  }, [role]);
+
   useEffect(() => {
     if (isUserError) {
       const statusCode = userError.response?.status;
       const errorCode = (userError.response?.data as { code?: string })?.code;
-      if (statusCode === 403 && errorCode === "ONBOARDING_ACCESS_ONLY") {
+      console.log(errorCode);
+      if (statusCode === 403) {
         router.push("/register");
       } else {
         alert("로그인에 실패했습니다. 다시 시도해주세요.");
